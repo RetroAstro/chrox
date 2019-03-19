9bbe6caf19dc3a5363b176d01891c8cf28433e5f
RetroAstro <452461114@qq.com>
2019-03-20 00:19:47 +0800

    1.0.3

--- examples/counter/index.js
+++ examples/counter/index.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { render } from 'react-dom'
-import createChrox from '../../dist/chrox'
+import createChrox from 'chrox'
 import { countReducer, initialState } from './reducer'
 
 const { Context, Provider } = createChrox(countReducer, initialState)
